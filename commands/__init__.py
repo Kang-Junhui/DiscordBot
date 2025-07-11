d0d4f76fc2145d973d8548334c01798d32eb973e
Kang-Junhui <72507070+Kang-Junhui@users.noreply.github.com>
2025-07-11 13:47:10 +0900

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,6 +2,13 @@ import os
 import asyncio
 
 async def load_all_commands(bot):
-    for filename in os.listdir(os.path.dirname(__file__)):
-        if filename.endswith(".py") and filename != "__init__.py" and filename != "customHelp.py":
-            await bot.load_extension(f"commands.{filename[:-3]}")
+    # white list 파일을 참조 후 command로 쓸 .py 파일을 load_extension 
+    white_list = os.path.join(os.path.dirname(__file__), "WhiteList.txt")
+    with open(white_list, 'r', encoding='utf-8') as f:
+        module = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+    for mod in module:
+        try:
+            await bot.load_extension(f"commands.{mod[:-3]}")
+        except Exception as e:
+            print(f"Failed to load {mod}: {e}")
